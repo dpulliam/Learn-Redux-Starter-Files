899585f76cc072c0b30a6ddb10d2c32c444322bc
Dylan Pulliam <dpulliam@customink.com>
2016-08-10 16:37:34 -0400

    Lesson 14

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -8,10 +8,12 @@ const Single = React.createClass({
 
     const i = this.props.posts.findIndex((post) => post.code === postId);
     const post = this.props.posts[i];
+    const postComments = this.props.comments[postId] || [];
+
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props} />
-        <Comments />
+        <Comments postComments={postComments}/>
       </div>
     )
   }
