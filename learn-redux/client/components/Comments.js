25ebd7da24a6a126628119156d8c8e0d6054a75e
Dylan Pulliam <dpulliam@customink.com>
2016-08-10 17:08:32 -0400

    Lesson 16

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Comments = React.createClass({
   renderComment(comment, i) {
     return (
-      <div className="comment" key={i} >
+      <div className="comment" key={i}>
         <p>
-          <strong> {comment.user} </strong>
+          <strong>{comment.user}</strong>
           {comment.text}
-          <button className="remove-comment" >&times;</button>
+          <button className="remove-comment" onClick={this.props.removeComment.bind(null, this.props.params.postId, i)}>&times;</button>
         </p>
       </div>
     )
@@ -17,15 +17,16 @@ const Comments = React.createClass({
     const { postId } = this.props.params;
     const author = this.refs.author.value;
     const comment = this.refs.comment.value;
-    this.props.addComent(postId,author,comment);
+    this.props.addComment(postId, author, comment);
+    this.refs.commentForm.reset();
   },
   render() {
     return (
       <div className="comments">
         {this.props.postComments.map(this.renderComment)}
-        <form onSubmit={this.handleSubmit} ref="commentForm" className="comment-form">
-          <input type="text" ref="author" placeholder="author" />
-          <input type="text" ref="comment" placeholder="comment" />
+        <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
+          <input type="text" ref="author" placeholder="author"/>
+          <input type="text" ref="comment" placeholder="comment"/>
           <input type="submit" hidden />
         </form>
       </div>
