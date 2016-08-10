d9d677cf4f8160e47524a099d5c443ee400f83e4
Dylan Pulliam <dpulliam@customink.com>
2016-08-10 14:16:26 -0400

    Lesson 4

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,5 +1,5 @@
-// let's go!
 import React from 'react';
+
 import { render } from 'react-dom';
 
 // Import css
@@ -7,5 +7,19 @@ import css from './styles/style.styl';
 
 // Import Components
 import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
-render(<Main/>, document.getElementById('root'));
+// Import react router dependencies
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path='/' component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path='/view/:postId' component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'));
